220f49f02566613637e91d6343a80ec7aaf8a86c
Alexandr Kostiuchenko <x07.05.18@yandex.ru>
2025-08-12 22:22:05 +0300

    2508122222

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
@@ -6,7 +6,6 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.dal.mappers.UserRowMapper;
 
